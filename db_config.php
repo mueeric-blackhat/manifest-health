b5abb80d0fe12a0d0bb86b19a26bbbd5f8f0b993
Lian <ericmue345@gmail.com>
2025-03-21 09:35:58 +0300

    updated database

--- db_config.php
+++ db_config.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "assignment";
+$host = "b7sis8unsy5wdnmylrgg-mysql.services.clever-cloud.com"; // Clever Cloud Host
+$username = "uom5oubv6loln5lw"; // Clever Cloud Username
+$password = "7fM3ORb85nEdcDbjwVn8"; // Clever Cloud Password
+$database = "b7sis8unsy5wdnmylrgg"; // Clever Cloud Database Name
+$port = 3306; // MySQL Port
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-// Check the connection
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
